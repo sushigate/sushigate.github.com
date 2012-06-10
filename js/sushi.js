5b1a94cf00bdd118727dcf5fbf03e568d43ddd6b
Go Tanaka <tanago3@gmail.com>
2012-06-10 22:40:57 +0900

    fixed scroll position

--- js/sushi.js
+++ js/sushi.js
@@ -1,10 +1,6 @@
 $(function(){
   var distance = $('.roll').offset();
-<<<<<<< HEAD
   $("html,body").animate({ scrollTop : distance.top + 660 }, 26000, "linear", function(){
-=======
-  $("html,body").animate({ scrollTop : distance.top + 650 }, 25000, "linear", function(){
->>>>>>> f5555b459fb823fc0d180ec0baa930321e24a048
     $('.dish').fadeTo('slow', 0.5);
   });
 });
