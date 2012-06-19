a4fc776012b96a294c193fed9cc7b951f0140103
Go Tanaka <tanago3@gmail.com>
2012-06-19 21:55:21 +0900

    added issps2009

--- js/sushi.js
+++ js/sushi.js
@@ -1,6 +1,6 @@
 $(function(){
   var distance = $('.roll').offset();
-  $("html,body").animate({ scrollTop : distance.top + 695 }, 28000, "linear", function(){
+  $("html,body").animate({ scrollTop : distance.top + 735 }, 29000, "linear", function(){
     $('.dish').fadeTo('slow', 0.5);
   });
 });
