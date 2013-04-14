ad7f5deec87b2a33b65e8590bb9d13dd931b0d0c
Go Tanaka <tanago3@gmail.com>
2013-04-15 00:58:10 +0900

    added @eiryu

--- js/sushi.js
+++ js/sushi.js
@@ -1,6 +1,6 @@
 $(function(){
   var distance = $('.roll').offset();
-  $("html,body").animate({ scrollTop : distance.top + 840 }, 32000, "linear", function(){
-    $('.dish').fadeTo('slow', 0.5);
+  $("html,body").animate({ scrollTop : distance.top + 900 }, 30000, "linear", function(){
+    $('.dish').fadeTo('slow', 0.3);
   });
 });
