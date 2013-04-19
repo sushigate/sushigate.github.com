2775145894a1d801e25cae4c7f8a740e4a0d9d63
Go Tanaka <tanago3@gmail.com>
2013-04-20 00:24:50 +0900

    added @soundTricker318.

--- js/sushi.js
+++ js/sushi.js
@@ -1,6 +1,6 @@
 $(function(){
   var distance = $('.roll').offset();
-  $("html,body").animate({ scrollTop : distance.top + 940 }, 30000, "linear", function(){
+  $("html,body").animate({ scrollTop : distance.top + 960 }, 30000, "linear", function(){
     $('.dish').fadeTo('slow', 0.3);
   });
 });
