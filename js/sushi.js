bf7cbc3f814fddd627cc9d5467bc2aceb04f512b
Go Tanaka <tanago3@gmail.com>
2013-04-26 01:06:08 +0900

    added @yamadamn.

--- js/sushi.js
+++ js/sushi.js
@@ -1,6 +1,6 @@
 $(function(){
   var distance = $('.roll').offset();
-  $("html,body").animate({ scrollTop : distance.top + 960 }, 30000, "linear", function(){
+  $("html,body").animate({ scrollTop : distance.top + 999 }, 30000, "linear", function(){
     $('.dish').fadeTo('slow', 0.3);
   });
 });
