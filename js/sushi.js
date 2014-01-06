0534d250ca0df9848a127a8e691c95553d2e83b7
Go Tanaka <tanaka@plugram.co.jp>
2014-01-06 19:40:25 +0900

    added @esehara.

--- js/sushi.js
+++ js/sushi.js
@@ -1,6 +1,6 @@
 $(function(){
   var distance = $('.roll').offset();
-  $("html,body").animate({ scrollTop : distance.top + 1240 }, 30000, "linear", function(){
+  $("html,body").animate({ scrollTop : distance.top + 1280 }, 30000, "linear", function(){
     $('.dish').fadeTo('slow', 0.3);
   });
 });
